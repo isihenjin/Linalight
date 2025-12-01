8fc29b81b28b4d4b0981b13404947726c2095120
Kokoro Kato <146074824+isihenjin@users.noreply.github.com>
2025-12-01 17:14:52 +0900

    initial commit

---
+++ Linalight/src-tauri/src/main.rs
@@ -0,0 +1,10 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+use tauri_plugin_sql::{Builder as SqlBuilder};
+
+fn main() {
+    tauri::Builder::default()
+        .plugin(SqlBuilder::default().build())
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
